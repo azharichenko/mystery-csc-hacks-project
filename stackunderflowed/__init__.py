32577387d58dc9d62c4a3e4ba801681900912b5f
Alex Zharichenko <azharichenko@gmail.com>
2021-10-09 11:18:03 -0400

    Added start of distrbutor and new refactor for fetching current state and new models

--- stackunderflowed/__init__.py
+++ stackunderflowed/__init__.py
@@ -1,39 +1,61 @@
 import json
 from pathlib import Path
-from typing import Optional
+from typing import NamedTuple, Optional
 
-from algosdk.v2client import algod
-from algosdk import mnemonic
-from algosdk.v2client import indexer
+from stackunderflowed.models import connect_or_init_db
 
-# Data directory and configuration file stuff
-from stackunderflowed.state import load_current_project_state, fetch_configuration
+DATA_DIR: Path = Path.cwd() / "data"
+CONFIG_FILE: Path = DATA_DIR / "config.json"
 
 
-ALGOD_ADDRESS = "https://testnet-algorand.api.purestake.io/ps2"
-INDEXER_ADDRESS = "https://testnet-algorand.api.purestake.io/idx2"
+class ProjectConfiguration(NamedTuple):
+    api_key: str = "Insert Algorand node API key here"
+    custodian_wallet_mnemonic: str = "Insert custodian wallet mnemonic phrase here"
+    work_token_id: int = -1
+    experience_token_id: int = -1
+
+
+config: Optional[ProjectConfiguration] = None
+
+
+def _init_configuration_file() -> None:
+    print("Please fill out the config.json file in the newly created data directory")
+    with CONFIG_FILE.open("w") as f:
+        json.dump(ProjectConfiguration()._asdict(), f, indent=4)
+
+
+def fetch_configuration() -> ProjectConfiguration:
+    global config
+    if config is None:
+        raise RuntimeError("Project configuration hasn't been loaded")
+    return config
+
+
+def load_current_project_state() -> bool:
+    """Loads in current configuration and SQLite database
+    If project state is missing the project will initialize it
+    and request the user fill out the configuration file before
+    rerunning the script again.
+    """
+    if not DATA_DIR.exists():
+        DATA_DIR.mkdir()
+        _init_configuration_file()
+        return False
+
+    if not CONFIG_FILE.exists():
+        _init_configuration_file()
+        return False
+
+    global config
+    with CONFIG_FILE.open("r") as f:
+        config = ProjectConfiguration(**json.load(f))
+
+    connect_or_init_db()
+
+    return True
 
 
 checked = load_current_project_state()
 
 if not checked:
     exit()
-
-
-config = fetch_configuration()
-
-# Setup HTTP client w/guest key provided by PureStake
-algod_token = config.api_key
-purestake_token = {"X-Api-key": algod_token}
-headers = {
-    "X-API-Key": algod_token,
-}
-
-# Initalize throw-away account for this example - check that is has funds before running script
-mnemonic_phrase = config.custodian_wallet_mnemonic
-account_private_key = mnemonic.to_private_key(mnemonic_phrase)
-account_public_key = mnemonic.to_public_key(mnemonic_phrase)
-
-# One client to rule them all
-indexer_client = indexer.IndexerClient("", INDEXER_ADDRESS, headers)
-algod_client = algod.AlgodClient(algod_token, ALGOD_ADDRESS, headers=purestake_token)
--- stackunderflowed/wallets.py
+++ stackunderflowed/wallets.py
@@ -1,2 +1,14 @@
+from algosdk import mnemonic
+
+from stackunderflowed import fetch_configuration
+
+config = fetch_configuration()
+
+# Initalize throw-away account for this example - check that is has funds before running script
+mnemonic_phrase = config.custodian_wallet_mnemonic
+account_private_key = mnemonic.to_private_key(mnemonic_phrase)
+account_public_key = mnemonic.to_public_key(mnemonic_phrase)
+
+
 def load_main_wallet() -> None:
     pass
