173ab8c73809bd3a658dcb12fcda4071bd9ff106
Alex Zharichenko <azharichenko@gmail.com>
2021-10-28 11:52:46 -0400

    Adding in base class for stateful apps and beginning to introduce a new way of deploying contracts

--- mechanics/script_purchase_contract.py
+++ mechanics/script_purchase_contract.py
@@ -1,63 +1,106 @@
 """
 Contract for Purchasing Simple Script tokens using Grind Toekns
 """
-from pyteal import *
+from pyteal import (
+    Seq,
+    Return,
+    App,
+    Bytes,
+    Int,
+    And,
+    Global,
+    Txn,
+    If,
+    ScratchVar,
+    TealType,
+    Cond,
+    OnComplete,
+    Mode,
+    compileTeal,
+)
+
+from mechanics import StatefulApplication
 
 
-def contract_program():
-    on_creation = Seq([App.globalPut(Bytes("Count"), Int(0)), Return(Int(1))])
+class SimpleScriptPurchaseApp(StatefulApplication):
+    def __init__(self) -> None:
+        pass
 
-    handle_optin = Return(Int(0))
+    def on_creation(self):
+        return Seq([App.globalPut(Bytes("Count"), Int(0)), Return(Int(1))])
 
-    handle_closeout = Return(Int(0))
+    def handle_optin(self):
+        return Return(Int(0))
 
-    handle_updateapp = Return(Int(0))
+    def handle_closeout(self):
+        return Return(Int(0))
 
-    handle_deleteapp = Return(Int(0))
+    def handle_updateapp(self):
+        return Return(Int(0))
 
-    scratchCount = ScratchVar(TealType.uint64)
+    def handle_deleteapp(self):
+        return Return(Int(0))
 
-    add = Seq(
-        [
-            scratchCount.store(App.globalGet(Bytes("Count"))),
-            App.globalPut(Bytes("Count"), scratchCount.load() + Int(1)),
-            Return(Int(1)),
-        ]
-    )
+    def handle_noop(self):
+        scratchCount = ScratchVar(TealType.uint64)
 
-    deduct = Seq(
-        [
-            scratchCount.store(App.globalGet(Bytes("Count"))),
-            If(
-                scratchCount.load() > Int(0),
-                App.globalPut(Bytes("Count"), scratchCount.load() - Int(1)),
-            ),
-            Return(Int(1)),
-        ]
-    )
+        add = Seq(
+            [
+                scratchCount.store(App.globalGet(Bytes("Count"))),
+                App.globalPut(Bytes("Count"), scratchCount.load() + Int(1)),
+                Return(Int(1)),
+            ]
+        )
 
-    handle_noop = Cond(
-        [
-            And(Global.group_size() == Int(1), Txn.application_args[0] == Bytes("Add")),
-            add,
-        ],
-        [
-            And(
-                Global.group_size() == Int(1),
-                Txn.application_args[0] == Bytes("Deduct"),
-            ),
-            deduct,
-        ],
-    )
+        deduct = Seq(
+            [
+                scratchCount.store(App.globalGet(Bytes("Count"))),
+                If(
+                    scratchCount.load() > Int(0),
+                    App.globalPut(Bytes("Count"), scratchCount.load() - Int(1)),
+                ),
+                Return(Int(1)),
+            ]
+        )
 
-    program = Cond(
-        [Txn.application_id() == Int(0), on_creation],
-        [Txn.on_completion() == OnComplete.OptIn, handle_optin],
-        [Txn.on_completion() == OnComplete.CloseOut, handle_closeout],
-        [Txn.on_completion() == OnComplete.UpdateApplication, handle_updateapp],
-        [Txn.on_completion() == OnComplete.DeleteApplication, handle_deleteapp],
-        [Txn.on_completion() == OnComplete.NoOp, handle_noop],
-    )
+        handle_noop = Cond(
+            [
+                And(
+                    Global.group_size() == Int(1),
+                    Txn.application_args[0] == Bytes("Add"),
+                ),
+                add,
+            ],
+            [
+                And(
+                    Global.group_size() == Int(1),
+                    Txn.application_args[0] == Bytes("Deduct"),
+                ),
+                deduct,
+            ],
+        )
 
-    # Mode.Application specifies that this is a smart contract
-    return compileTeal(program, Mode.Application, version=4)
+        return handle_noop
+
+    @property
+    def approval_program(self):
+        program = Cond(
+            [Txn.application_id() == Int(0), self.on_creation()],
+            [Txn.on_completion() == OnComplete.OptIn, self.handle_optin()],
+            [Txn.on_completion() == OnComplete.CloseOut, self.handle_closeout()],
+            [
+                Txn.on_completion() == OnComplete.UpdateApplication,
+                self.handle_updateapp(),
+            ],
+            [
+                Txn.on_completion() == OnComplete.DeleteApplication,
+                self._handle_deleteapp(),
+            ],
+            [Txn.on_completion() == OnComplete.NoOp, self.handle_noop()],
+        )
+        return compileTeal(program, Mode.Application, version=4)
+
+    @property
+    def clear_state_program(self):
+        program = Return(Int(1))
+        return compileTeal(program, Mode.Application, version=4)
